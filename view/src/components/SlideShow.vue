759f54ef86392cb689c4b4dcbb8ccc9ed73c2cee
dzw <zhiwen_der@qq.com>
2022-03-23 23:40:22 +0800

    添加首页轮播图组件开发

--- view/src/components/SlideShow.vue
+++ view/src/components/SlideShow.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="slide-show">
-    <div>
-
+    <div class="slide-left">
+     <img class="slide-left-img" src="../../public/imgs/1.webp">
     </div>
-      <div class="slide" v-for="item in pictures" :key="item">
-        {{item}}
-      </div>
-    <div>
-
+    <div class="slide">
+      <img class="slide-img" src="../../public/imgs/2.webp">
+    </div>
+    <div class="slide-right">
+      <img class="slide-right-img" src="../../public/imgs/3.webp">
     </div>
   </div>
 </template>
@@ -20,25 +20,53 @@ export default {
 </script>
 
 <style scoped>
+
 .slide-show{
   display: flex;
   flex-direction: row;
 }
-.slide{
-  width: 400px;
-  height: 200px;
-  background-color: #dbd5d5;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-  margin-left: 5px;
+
+.slide-left-img{
+  width: 500px;
+  height:200px;
+  opacity: 0.5;
+}
+
+.slide-img{
+  width: 500px;
+  height:250px;
+}
+
+.slide-right-img{
+  width: 500px;
+  height:200px;
+  opacity: 0.5;
 }
 
 .slide-left{
+  width: 300px;
+  height:250px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  overflow: hidden;
+
+}
+.slide{
+  width: 500px;
+  height:250px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 
 }
 .slide-right{
-
+  width: 300px;
+  height:250px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  overflow: hidden;
 }
+
 </style>
--- view/src/views/page/IndexView.vue
+++ view/src/views/page/IndexView.vue
@@ -16,7 +16,13 @@ export default {
   },
   data(){
     return{
-      pictures:[1,2,3]
+      pictures:[
+          'https://i0.hdslb.com/bfs/feed-admin/b00f9120d7ca701195d03503566b03b14baf240d.jpg@1200w_300h_1c.webp',
+          'https://i0.hdslb.com/bfs/feed-admin/9cb02a15c3d486bb952ecf34544fae873f5d5f36.png@1200w_300h_1c.webp',
+          'https://i0.hdslb.com/bfs/feed-admin/25df7b226f64520e52df766a4b203c5b06dfc21d.jpg@1200w_300h_1c.webp',
+          'https://i0.hdslb.com/bfs/feed-admin/e7d4dcd6aa3ead0a1be7546eb3bff75058323da7.jpg@1200w_300h_1c.webp',
+          'https://i0.hdslb.com/bfs/feed-admin/b15a2dbff3a4cbc59c481e5d539c027304f91908.jpg@1200w_300h_1c.webp'
+      ]
     }
   },
 }
