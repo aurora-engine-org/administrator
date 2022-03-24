ee2955874fa023d0b426d042e6db90cd2c132fa6
dzw <hu.duanzhiwen@aisino.com>
2022-03-24 10:05:20 +0800

    修改轮播图

--- view/src/components/SlideShow.vue
+++ view/src/components/SlideShow.vue
@@ -1,13 +1,27 @@
 <template>
-  <div class="slide-show">
-    <div class="slide-left">
-     <img class="slide-left-img" src="../../public/imgs/1.webp">
+  <div class="slide">
+    <div class="slide-left-box">
+      <div class="left-img-box">
+        <div class="left-img">
+          <img class="limg" src="../../public/imgs/1.webp">
+        </div>
+      </div>
     </div>
-    <div class="slide">
-      <img class="slide-img" src="../../public/imgs/2.webp">
+    <div class="slide-center-box">
+      <div :class="center-img-box" @click="check">
+        <div class="center-img">
+          <transition name="center">
+            <img class="cimg" :src="require('../../public/imgs/'+this.pictures[checkIndex])">
+          </transition>
+        </div>
+      </div>
     </div>
-    <div class="slide-right">
-      <img class="slide-right-img" src="../../public/imgs/3.webp">
+    <div class="slide-right-box">
+      <div class="right-img-box">
+        <div class="right-img">
+          <img class="rimg" src="../../public/imgs/3.webp">
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -16,57 +30,74 @@
 export default {
   name: "SlideShow",
   props:['pictures'],
+  data(){
+    return{
+      index:0
+    }
+  },
+  methods:{
+    check(){
+      if (this.index<this.pictures.length){
+        this.index++
+      }else {
+        this.index=0
+      }
+    }
+  },
+  computed:{
+    checkIndex(){
+      console.log(this.index)
+      return this.index
+    }
+  }
 }
 </script>
 
 <style scoped>
-
-.slide-show{
+.slide{
+  width: 1000px;
+  height: 300px;
   display: flex;
   flex-direction: row;
+  justify-content: center;
 }
 
-.slide-left-img{
-  width: 500px;
-  height:200px;
-  opacity: 0.5;
+/*整体最外层大小一致，让里面的div垂直居中*/
+.slide-center-box,.slide-left-box,.slide-right-box{
+  width: 400px;
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 }
 
-.slide-img{
-  width: 500px;
-  height:250px;
-}
-
-.slide-right-img{
-  width: 500px;
-  height:200px;
-  opacity: 0.5;
-}
-
-.slide-left{
+/*左右两侧图片盒子高度略低于中间*/
+.left-img-box,.right-img-box{
   width: 300px;
-  height:250px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  overflow: hidden;
-
-}
-.slide{
-  width: 500px;
-  height:250px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-
-}
-.slide-right{
-  width: 300px;
-  height:250px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+  height: 250px;
   overflow: hidden;
 }
+
+/*图片样式*/
+.cimg{
+  width: 400px;
+  height: 300px;
+}
+.limg,.rimg{
+  width: 400px;
+  height: 300px;
+  opacity: 0.3;
+}
+
+/*切换动画特效*/
+
+/*中间切换过度效果*/
+
+/*两端切换过度效果*/
+
 
 </style>
+
+
+
+
